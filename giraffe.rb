22b5a00cf9f3630b905607987543430574f923fc
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-02 17:40:06 +0200

    Variable

---
+++ giraffe.rb
@@ -0,0 +1,12 @@
+age = "35"
+name = "John"
+
+put ("Hello" + name + "always put parentheses even if you have " + age)
+
+# 1 #Just follow next steps in console terminal ;)
+# 2 git init #Initialize git in folder
+# 3 git add . #add all files of folder to be pushed
+# 4 git commit -m "First commit" #add first commit
+# 5 git remote add origin remote_repository_URL #replace with your remote repo url
+# 6 git remote -v #verify that your remote repository url is properly found
+# 7 git push --force origin master #force pushing your project into github repo
